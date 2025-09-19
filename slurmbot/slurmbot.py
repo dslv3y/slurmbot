d17f21a797e25bd92dbcf2d17901747fd95340ea
Dmitry Solovey <dsolovey@ist.ac.at>
2025-09-19 16:52:07 +0200

    change defaults

--- slurmbot/slurmbot.py
+++ slurmbot/slurmbot.py
@@ -24,13 +24,19 @@ class SlurmBot:
 		# Update parameters with defaults from config and provided kwargs
 		params = self.config.copy()
 
-		params.update(kwargs)  # kwargs override config defaults
+		params.update(kwargs)
+
+		if "dependency" in params.keys() and params["dependency"]:
+			if isinstance(params["dependency"], list):
+				params["dependency"] = [str(d) for d in params["dependency"]]
+				params["dependency"] = ",".join(params["dependency"])
+			params["dependency"] = f'--dependency=afterok:{params["dependency"]}'
+		else:
+			params["dependency"] =""
 
 		params["cmd"] = " " + cmd
 		params["prefix"] = params["prefix"] + " && " if "prefix" in params.keys() and params["prefix"] else "" 
 		params["conda"] = params["conda_prefix"] + " " + params["conda"] + " && " if "conda" in params.keys() and params["conda"] else "" 
-		# params["dependency"] = f'--dependency=afterok:{params["dependency"]}' if params["dependency"] else ""
-		# TODO add support for multiple dependencies
 		params["reservation"] = f'--reservation={params["reservation"]}' if params["reservation"] else ""
 		params["account"] = f'--account {params["account"]}' if params["account"] else ""
 		params["partition"] = f'--partition {params["partition"]}' if params["partition"] else ""
@@ -38,10 +44,10 @@ class SlurmBot:
 		params["time"] = params["time"] if "time" in params.keys() else 24
 		params["cpus"] = params["cpus"] if "cpus" in params.keys() else 4
 		params["mem"] = params["mem"] if "mem" in params.keys() else 4
-		params["name"] = params["name"] if "name" in params.keys() else cmd.split()[0]
+		params["name"] = params["name"] if ("name" in params.keys() and params["name"]) else cmd.split(" ")[0]
 
 		# Build sbatch command
-		sbatch_cmd = f'''sbatch {params["account"]} {params["partition"]} {params["reservation"]} \
+		sbatch_cmd = f'''sbatch {params["account"]} {params["partition"]} {params["reservation"]} {params["dependency"]} \
 -o '{params["logdir"]}/%j.out' -e '{params["logdir"]}/%j.err' --job-name={params["name"]} \
 -c {params["cpus"]} --mem={params["mem"]}G --time={params["time"]}:00:00 \
 --parsable --wrap "/bin/bash -c '{params["prefix"]}{params["conda"]}{params["cmd"]}'"'''
@@ -50,14 +56,12 @@ class SlurmBot:
 			print(f"\033[33mDry run: Command not submitted. Full sbatch command:\033[0m\n{sbatch_cmd}")
 			return
 
-		# Execute the command if not dry run
 		try:
 			if v > 1:
 				print(f"Submitting job with command: {sbatch_cmd}")
 			result = subprocess.run(sbatch_cmd, shell=True, check=True, capture_output=True, text=True)
 			job_id = result.stdout.strip()
-			if v >= 1:
-				print(job_id)
+			return job_id
 
 		except subprocess.CalledProcessError as e:
 			print(f"Error: {e.stderr}")
